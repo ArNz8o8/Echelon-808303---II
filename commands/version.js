afea547abd220ad1008944021fdfd884f948ba8a
ArNz|8o8 <adenhartog@me.com>
2021-01-24 18:55:01 +0100

    Update version.js

--- commands/version.js
+++ commands/version.js
@@ -4,6 +4,6 @@ module.exports = {
 	name: 'version',
 	description: 'Displayz the bot info.. really do not know why tho',
 	execute(message) {
-		message.channel.send(`this bot is made by 8o83o3Designz, version ${version_echelon} \nnothing much, but its something`);
+		message.channel.send(`Echelon (\`version ${version_echelon}\`) is made by ArNz8o8 from 8o83o3Designz inc, see ${prefix}stats for moar nerd shizzle`);
 	},
 };
