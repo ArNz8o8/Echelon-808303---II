9fc91b887f6c0b9b051c069e817d57021eeeb909
ArNz|8o8 <adenhartog@me.com>
2021-01-24 22:37:09 +0100

    Update addons.js

--- commands/addons.js
+++ commands/addons.js
@@ -1,16 +1,16 @@
 // WoW addons for Echelon 808303
-// Coded by ArNz8o8 on jan 24th 2021
-
+// Coded by ArNz8o8 on jan 21st 2021
 module.exports = {
-	name: 'addons',
-	aliases: ["addon", "wut"],
-	description: "Check what addons you need for Warcraft",
-	cooldown: 5,
-	execute(message, client) {
-	
-	message.channel.send('Looking for addons on curseforge...').then((msg)=> {
-  setTimeout(function(){
-	msg.edit(`Download these addons:\n\nDetails : https://www.curseforge.com/wow/addons/details\nDeadly Boss Mods: https://www.curseforge.com/wow/addons/deadly-boss-mods\nWorld Quest Tracker: https://www.curseforge.com/wow/addons/world-quest-tracker`);
-		}, 5000)
-	},
+    name: 'addons',
+    aliases: ["addon", "wut"],
+    description: "Check what addons you need for Warcraft",
+    cooldown: 5,
+    execute(message, client) {
+
+        message.channel.send('Looking for addons on curseforge...').then((msg) => {
+            setTimeout(function() {
+                msg.edit(`Download these addons:\n\n**Details:** https://www.curseforge.com/wow/addons/details\n**Deadly Boss Mods:** https://www.curseforge.com/wow/addons/deadly-boss-mods\n**World Quest Tracker:** https://www.curseforge.com/wow/addons/world-quest-tracker\n`);
+            }, 5000)
+        });
+    }
 }
