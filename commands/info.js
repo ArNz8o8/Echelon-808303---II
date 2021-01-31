a19feb3a8d2c7846351984d029c07ca5f54ec449
ArNz|8o8 <adenhartog@me.com>
2021-01-31 21:08:52 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -7,36 +7,32 @@ module.exports = {
 	aliases: ['commands'],
 	usage: '[command name]',
 	cooldown: 5,
-	execute(message, args) {
+	execute(message, args, client) {
 		const data = [];
 		const { commands } = message.client;
-		
-		if (!args.length) {
-		const title = 'Hi! So yeah, my name is Echelon\nand here\'s a list of "all" my commands';
-		const description = data.push(commands.map(command => command.name).join(', '));
-		const footer = `🔥 Brought to you by ArNz8o8`;
-		const infoEmbed = new Discord.MessageEmbed()
-		.setColor('#FF8315')
-		.setTitle(title)
-		.setDescription(data)
-		.setThumbnail(`https://i.imgur.com/mhQeaaX.png`)
-		.setFooter(footer);
-		return message.author.send(infoEmbed)
-			.then(() => {
-			if (message.channel.type === 'dm') return;
-			message.reply('Imma slide into your DM with all of me *wink*').then(msg => {msg.delete({ timeout: 10000 });
-				})
-	   .catch(error => {
-		   console.error(`Could not send info DM to ${message.author.tag}.\n`, error);
-		   message.reply('it seems like I can\'t DM you, wtd');
-			   });
-		 })
-			 const name = args[0].toLowerCase();
-			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
-			if (!command) {
-				return message.reply('that\'s not a valid command, lamer');
-			}
+		if (!args.length) {
+			data.push('Here\'s a list of all my commands:');
+			data.push(commands.map(command => command.name).join(', '));
+			data.push(`\nYou can send \`${prefix}info [command name]\` to get info on a specific command!`);
+
+			return message.author.send(data, { split: true })
+				.then(() => {
+					if (message.channel.type === 'dm') return;
+					message.reply('I will slide into your dm with all the intel')
+				})
+				.catch(error => {
+					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+					message.reply('it seems like I can\'t DM you!');
+				});
+		}
+
+		const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+		if (!command) {
+			return message.reply('that\'s not a valid command!');
+		}
 
 		data.push(`**Name:** ${command.name}`);
 
@@ -47,5 +43,5 @@ module.exports = {
 		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
 		message.channel.send(data, { split: true });
-		}
-  }}
+	},
+};
