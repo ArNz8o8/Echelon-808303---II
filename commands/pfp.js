8c8cb60ecc698e3981d821e339332382b2ba2b6d
ArNz|8o8 <adenhartog@me.com>
2021-05-05 09:01:56 +0200

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -1,15 +1,29 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+
 module.exports = {
-	name: 'pfp',
-	description: 'Get that profile picture of any user, for you know... research purposes.',
-	execute(message) {
-		if (!message.mentions.users.size) {
-			return message.channel.send(`Your profile pic: ${message.author.displayAvatarURL({ format: "png", dynamic: true })}`);
-		}
+    name: "pfp",
+    aliases: ["avatar", "profilepic"],
+    category: "info",
+    description: "Get that profile picture of any user, for you know... research purposes.",
+    usage: "!pfp <@username> (if only the command !pfp is used, you get your own avatar",
 
-		const pfpList = message.mentions.users.map(user => {
-			return `${user.username}'s profile pic: ${user.displayAvatarURL({ dynamic: true })}`;
-		});
+    execute: async (message, args) => {
 
-		message.channel.send(pfpList);
-	},
-};
+
+        let member = message.mentions.users.first() || message.author
+
+        let avatar = member.displayAvatarURL({
+            size: 1024
+        })
+
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`Here you go, ${member.username}'s avatar`)
+            .setImage(avatar)
+            .setColor("#FF8315")
+
+            .setFooter('Avatar lookup coded by ArNz8o8 🔥', 'https://i.imgur.com/R56xBE3.png')
+        message.channel.send(embed);
+    }
+}
